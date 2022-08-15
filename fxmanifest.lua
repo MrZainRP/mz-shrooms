e6582f1f14ca65156ecacf98bdd6d5280c03bbad
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2022-08-16 01:17:06 +1000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ fx_version 'adamant'
 game 'gta5'
 
 description 'A resource for the picking and packaging of mushrooms built for qb-core - for use with mz-skills'
-version '1.0.0'
+version '1.1.0'
 author 'Mr_Zain#4139'
 
 shared_scripts {
